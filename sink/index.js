5c3a0ba729bbeff1ce6f5e1034f51f0c7cbe36f3
satnam2609 <satnamm143@gmail.com>
2025-08-19 23:00:28 +0530

    added pino lib for logging

--- sink/index.js
+++ sink/index.js
@@ -2,18 +2,34 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { createClient } = require("redis");
+const pino = require("pino");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json()); // <-- Parse JSON body
+app.use(express.json());
 
-const client = createClient({
-  url: process.env.REDIS_URL || "redis://redis:6379"
+const logger = pino({
+  level: process.env.LOG_LEVEL || "info",  
+  transport: {
+    target: "pino-pretty", 
+    options: {
+      colorize: true,
+      translateTime: "SYS:standard",
+      ignore: "pid,hostname",
+    },
+  },
 });
 
-client.connect().then(() => console.log("Redis connected")).catch(console.error);
+const client = createClient({
+  url: process.env.REDIS_URL || "redis://redis:6379",
+});
+
+client
+  .connect()
+  .then(() => console.log("Redis connected"))
+  .catch(console.error);
 
 app.post("/sink", async (req, res) => {
   const noteId = req.headers["x-note-id"];
@@ -26,18 +42,18 @@ app.post("/sink", async (req, res) => {
   try {
     const result = await client.set(key, 1, { NX: true, EX: 86400 });
     if (result === "OK") {
-      console.log('Payload: '+JSON.stringify({noteId,idempotentKey:key}));
+      logger.info(`noteId:${noteId} and idempotency key: ${key} is sinked now`);
       return res.status(200).json({ ok: true, stored: true });
     } else {
-      console.log("Duplicate note ignored:", noteId);
+      logger.info("Duplicate note ignored:", noteId);
       return res.status(200).json({ ok: true, stored: false });
     }
   } catch (err) {
-    console.error("Redis error:", err);
+    logger.info("Redis error:", err);
     return res.status(500).json({ error: "Redis failure" });
   }
 });
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log(`Sink server listening on port ${process.env.PORT || 4000}`);
+  logger.info(`Sink server listening on port ${process.env.PORT || 4000}`);
 });
